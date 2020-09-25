5873589e57a0e27b82318eb817fb99060696dd81
Gaardsholt <lasse.gaardsholt@bestseller.com>
2020-09-25 12:11:55 +0200

    easy..

---
+++ README.md
@@ -0,0 +1,21 @@
+# Bitbucket2Github
+
+> Small script to migrate from Bitbucket to Github
+
+
+## Usage
+
+### Required environment variables
+| Name            | Example                                 |
+| --------------- | --------------------------------------- |
+| BITBUCKET_OWNER | your-company                            |
+| GITHUB_ORG      | new-company                             |
+| GITHUB_TOKEN    | a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t |
+| GITHUB_TEAM     | team-eagle                              |
+
+<br>
+
+### You just call it like so:
+```bash
+./migrate.sh repo1 repo2 repo3
+```
--- migrate.sh
+++ migrate.sh
@@ -1,29 +1,63 @@
 #!/bin/bash
+CURRENT_DIR=$(pwd)
+REPOS=$@
+error_count=0
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-export CURRENT_DIR=$(pwd)
-export GITHUB_TOKEN="<insert github token here>"
-export GITHUB_TEAM="bestone"
+
+if [ "$BITBUCKET_OWNER" = "" ]; then
+  echo -e "${RED}Missing Bitbucket owner !${NC}"
+  echo -e "${BLUE}Add BITBUCKET_OWNER as an environment variable\n${NC}"
+  error_count=$((error_count+1))
+fi
+
+if [ "$GITHUB_TOKEN" = "" ]; then
+  echo -e "${RED}Missing github token !${NC}"
+  echo -e "${BLUE}Add GITHUB_TOKEN as an environment variable\n${NC}"
+  error_count=$((error_count+1))
+fi
+
+if [ "$GITHUB_TEAM" = "" ]; then
+  echo -e "${RED}Missing github team name !${NC}"
+  echo -e "${BLUE}Add GITHUB_TEAM as an environment variable\n${NC}"
+  error_count=$((error_count+1))
+fi
+
+if [ "$GITHUB_ORG" = "" ]; then
+  echo -e "${RED}Missing github organization name !${NC}"
+  echo -e "${BLUE}Add GITHUB_ORG as an environment variable\n${NC}"
+  error_count=$((error_count+1))
+fi
+
+if [ "$REPOS" = "" ]; then
+  echo -e "${RED}No arguments was given !${NC}"
+  error_count=$((error_count+1))
+fi
+
+if [ $error_count -gt 0 ]; then
+  exit $error_count
+fi
 
 
-REPOS=bestone-bi2-vendor-ui
-
-for REPO_NAME in $(echo $REPOS | sed "s/,/ /g")
+for REPO_NAME in $REPOS
 do
   echo "$REPO_NAME"
 
-  GH_REPO="BESTSELLER/$REPO_NAME"
+  GH_REPO="$GITHUB_ORG/$REPO_NAME"
 
   # Create repo in Github
   mkdir /tmp/repos && cd /tmp/repos
   gh repo create $GH_REPO --private --team $GITHUB_TEAM -y
   curl --request PUT \
-    --url https://api.github.com/orgs/BESTSELLER/teams/$GITHUB_TEAM/repos/$GH_REPO \
+    --url https://api.github.com/orgs/$GITHUB_ORG/teams/$GITHUB_TEAM/repos/$GH_REPO \
     --header 'accept: application/vnd.github.v3+json' \
     --header 'authorization: token '$GITHUB_TOKEN \
     --data '{"permission":"admin"}'
 
   # Clone old repo
-  git clone --mirror git@bitbucket.org:bestsellerit/$REPO_NAME.git $CURRENT_DIR/repo
+  git clone --mirror git@bitbucket.org:$BITBUCKET_OWNER/$REPO_NAME.git $CURRENT_DIR/repo
 
   # Push to Github
   cd $CURRENT_DIR/repo
